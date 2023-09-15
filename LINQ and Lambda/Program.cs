47152b746c2b287852d1e2e5eb4e590653d1daca
Levente Major <leventemajor36@gmail.com>
2023-09-15 11:08:50 +0200

    Added Projection and FIltering

--- LINQ and Lambda/Program.cs
+++ LINQ and Lambda/Program.cs
@@ -24,16 +24,57 @@ namespace LINQ_and_Lambda
             new Data { X = 9, Y = 10 },
             };
 
+            //Select
             //Query syntax
             //var results = from item in items select item.X;
 
             //Labda syntax
             var results = items.Select(item => item.X);
 
+            //Projection
+            //Query syntax
+            //var dblresults = from item in items
+            //                 select new
+            //                 {
+            //                     X2 = item.X * 2,
+            //                     Y2 = item.Y * 2
+            //                 };
+
+            //Lambda syntax
+            var dblresults = items.Select(item => new
+            {
+                X2 = item.X * 2,
+                Y2 = item.Y * 2
+            });
+
+            //Filtering
+            //Query syntax
+            //var smlresult = from item in items where item.X < 6 select item.X;
+
+            //Lambda syntax
+            var smlresult = items.Where(item => item.X < 6).Select(item => item.X);
+
+            //Output
             foreach (var item in results)
             {
                 Console.WriteLine(item);
             }
+
+            Console.WriteLine();
+
+            foreach (var item in dblresults)
+            {
+                Console.WriteLine($"{item.X2} - {item.Y2}");
+            }
+
+            Console.WriteLine();
+
+            foreach (var item in smlresult)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
 
             Console.ReadKey();
         }
